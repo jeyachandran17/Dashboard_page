64e457ad7673484d05d137713bad049668fc80ae
jeyachandran17 <jeyachandran1733@gmail.com>
2023-08-28 23:42:12 +0530

    alter 3rd module part

--- src/App.jsx
+++ src/App.jsx
@@ -273,25 +273,26 @@ const datas = {
         <div className="column2">
           <div className="Illustrations">
             <div className='Illustrations_tittle'>
-              <p>Illustrations</p>
-              
+              <p>Illustrations</p>  
             </div>
             <div className='Illustrations_content'>
               <div className="Illustrations_pic">
                 <img src="https://startbootstrap.github.io/startbootstrap-sb-admin-2/img/undraw_posting_photo.svg" alt="Illustrations content picture" className="picture"/>
               </div>
-              <p className="txtcontent">Add some quality, svg illustrations to your project courtesy of unDraw, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
+              <div className="txtcontent">
+                <p>Add some quality, svg illustrations to your project courtesy of unDraw, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
+              </div>   
             </div>
           </div>
-            <div className='Development_Approach'>
-              <div className='Development_Approach_tittle'>
-                <p>Development Approach</p>
-              </div>
-              <div className="txtcontent">
-                <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
-                <p>Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
-              </div>
+          <div className='Development_Approach'>
+            <div className='Development_Approach_tittle'>
+              <p>Development Approach</p>
             </div>
+            <div className="txtcontent">
+              <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
+              <p>Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
